e622319dc22819fbdb0a91f41cee3c4e6c3281c4
Rupayan2005 <rupayanauddya2023@gmail.com>
2024-12-23 15:58:58 +0530

    login page

---
+++ client/src/app/login/page.jsx
@@ -0,0 +1,93 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import axios from "axios";
+import { signIn, useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FormEvent, useEffect, useState } from "react";
+
+export default function Signup() {
+
+  const [userData,setUserData]=useState({})
+  const [error,setError]=useState("")
+  const router=useRouter()
+  const {data:session, status}=useSession()
+
+  
+  useEffect(() => {
+    if (status === 'loading') return; // Wait until session status is known
+    if (session?.user) {
+      //@ts-ignore
+      if(session?.user.role==="student"){
+        router.push('/dashboard');}
+      else{
+      router.push('/teacher-dashboard');}
+    }
+  }, [session, status, router]);
+
+  async function handleSubmit(e){
+    
+    
+      e.preventDefault()
+      const result = await signIn('credentials', {
+        redirect: false,
+        //@ts-ignore
+        identifier: userData.email,
+        //@ts-ignore
+        password: userData.password,
+      });
+  
+      if (result?.error) {
+        if (result.error === 'CredentialsSignin') {
+          console.log({
+            title: 'Login Failed',
+            description: 'Incorrect username or password',
+            variant: 'destructive',
+          });
+        } else {
+          console.log({
+            title: 'Error',
+            description: result.error,
+            variant: 'destructive',
+          });
+        }
+
+      }
+     console.log(result)
+      if (result?.error) {
+        //@ts-ignore
+        setError(result.error)
+
+      }}
+    
+  
+    return (<>
+      <header className="p-4">
+        <Link href="/">Home</Link>
+      </header>
+      <main className="flex-1 flex items-center justify-center px-4 py-12">
+      <div className=" rounded-lg p-8 shadow">
+            <h2 className="text-2xl font-bold mb-4">Login</h2>
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              
+              <div>
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required onChange={(e)=>{setUserData({...userData,email:e.target.value})}} />
+              </div>
+              <div>
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" type="password" required onChange={(e)=>{setUserData({...userData,password:e.target.value})}}/>
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              {error.length>0 && <div className="">{error}</div>}
+            </form>
+          </div>
+          </main>
+      </>
+    )
+  }
+  
